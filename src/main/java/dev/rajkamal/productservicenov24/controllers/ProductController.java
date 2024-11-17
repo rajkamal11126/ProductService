97778ff6c25e213541ec14db4a6c8831bc917e9f
Raj Kamal <rajkamal11126@gmail.com>
2024-11-17 14:43:26 +0530

    Added Controller Advice Exception Handler

--- src/main/java/dev/rajkamal/productservicenov24/controllers/ProductController.java
+++ src/main/java/dev/rajkamal/productservicenov24/controllers/ProductController.java
@@ -73,15 +73,13 @@ public class ProductController {
                 createProductRequestDto.getCategory());
     }
 
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<ErrorDTO> handleProductNotFoundException(ProductNotFoundException productNotFoundException){
-        ErrorDTO errorDTO = new ErrorDTO();
-
-
-        errorDTO.setMessage(productNotFoundException.getMessage());
-        ResponseEntity<ErrorDTO> responseEntity = new ResponseEntity<>(errorDTO, HttpStatus.NOT_FOUND);
-        return responseEntity;
-    }
+//    @ExceptionHandler(ProductNotFoundException.class)
+//    public ResponseEntity<ErrorDTO> handleProductNotFoundException(ProductNotFoundException productNotFoundException){
+//        ErrorDTO errorDTO = new ErrorDTO();
+//        errorDTO.setMessage(productNotFoundException.getMessage());
+//        ResponseEntity<ErrorDTO> responseEntity = new ResponseEntity<>(errorDTO, HttpStatus.NOT_FOUND);
+//        return responseEntity;
+//    }
 
     @PutMapping("/products/{id}")
     public Product updateProduct(@RequestBody CreateProductRequestDto requestDto, @PathVariable long id){
